ba023a3709f7bc5b7a1871e6cb3627c2d2f319b5
silliamh78 <har20041@byui.edu>
2026-05-02 16:33:33 -0600

    overlap hopefully

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,14 @@ function updateParkInformation(){
 }
 
 updateParkInformation()
+
+function loadParkData() {
+
+  document.getElementById("parkName").textContent = "Yellowstone";
+  document.getElementById("parkType").textContent = "National Park";
+  document.getElementById("parkStates").textContent = "WY, ID, MT";
+  document.querySelector("#park-image").src = "./images/yellowstone.jpg";
+
+}
+
+loadParkData();

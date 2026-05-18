23a81d583f61d828fe39fcf35382b37b51920971
silliamh78 <har20041@byui.edu>
2026-05-18 09:49:56 -0600

    let

--- src/js/key-test.js
+++ src/js/key-test.js
@@ -2,16 +2,11 @@
 const apiKey = import.meta.env.VITE_NPS_API_KEY;
 
 // Build the endpoint URL
-const url = "https://developer.nps.gov/api/v1/parks?parkCode=yell";
-
+let url = "https://developer.nps.gov/api/v1/parks?parkCode=yell";
+url=`${url}&api_key=${apiKey}`;
 // Fetch data using request headers
 async function getPark() {
-  const response = await fetch(url, {
-    headers: {
-      "X-Api-Key": apiKey
-    }
-  });
-
+  const response = await fetch(url);
   const data = await response.json();
 
   console.log(data);

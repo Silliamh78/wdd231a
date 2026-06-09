8ad6e8f890f4c0377fb18345ef425b225b121aea
silliamh78 <har20041@byui.edu>
2026-06-09 14:42:46 -0600

    linting try 2

--- src/js/key-test.js
+++ src/js/key-test.js
@@ -1,24 +1,24 @@
-// Get API key from the .env file
-const apiKey = import.meta.env.VITE_NPS_API_KEY;
+// // Get API key from the .env file
+// const apiKey = import.meta.env.VITE_NPS_API_KEY;
 
-// Build the endpoint URL
-const url = "https://developer.nps.gov/api/v1/parks?parkCode=yell";
-// url=`${url}&api_key=${apiKey}`;
-// // Fetch data using request headers
-async function getPark() {
-  const response = await fetch(url,
-    {
-      headers: {
-"X-Api-Key": apiKey,
-}
-    });
-  const data = await response.json();
-  data= JSON.parse(text)
+// // Build the endpoint URL
+// const url = "https://developer.nps.gov/api/v1/parks?parkCode=yell";
+// // url=`${url}&api_key=${apiKey}`;
+// // // Fetch data using request headers
+// async function getPark() {
+//   const response = await fetch(url,
+//     {
+//       headers: {
+// "X-Api-Key": apiKey,
+// }
+//     });
+//   let data = await response.json();
+//   data= JSON.parse(text)
 
-  console.log(data);
-  const parkTag = document.getElementById("park-info")
-  parkTag.innerText = JSON.stringify(data)
+//   console.log(data);
+//   let parkTag = document.getElementById("park-info")
+//   parkTag.innerText = JSON.stringify(data)
 
-}
+// }
 
-getPark();
+// getPark();
